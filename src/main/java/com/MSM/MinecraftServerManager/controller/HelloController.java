5697f55cbd9c9566dcb8412b4512857738a12705
PE_commander <1004210112@qq.com>
2022-03-25 13:14:03 +0800

    初版

---
+++ src/main/java/com/MSM/MinecraftServerManager/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.MSM.MinecraftServerManager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    // 测试用接口
+    @RequestMapping("/hello")
+    public String hello(){
+        return "哈喽！";
+    }
+}
